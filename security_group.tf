5c2f59df7cc60be3ccb8eea2d07ed1a655b3d875
danhill <danhill@digital.uc.dwp.gov.uk>
2020-12-07 15:42:56 +0000

    only generate egress rules. Use CIDR range instead of security groups
    Signed-off-by: danhill <danhill@digital.uc.dwp.gov.uk>

--- security_group.tf
+++ security_group.tf
@@ -47,24 +47,13 @@ resource "aws_security_group" "claimant_api_kafka_consumer" {
   )
 }
 
-resource "aws_security_group_rule" "ingress" {
-  for_each                 = { for security_group_rule in local.security_group_rules : security_group_rule.port => security_group_rule }
-  description              = "Allow inbound requests from ${each.value.name}"
-  type                     = "ingress"
-  from_port                = each.value.port
-  to_port                  = each.value.port
-  protocol                 = each.value.protocol
-  security_group_id        = each.value.destination
-  source_security_group_id = aws_security_group.claimant_api_kafka_consumer.id
-}
-
 resource "aws_security_group_rule" "egress" {
-  for_each                 = { for security_group_rule in local.security_group_rules : security_group_rule.port => security_group_rule }
-  description              = "Allow outbound requests to ${each.value.name}"
-  type                     = "egress"
-  from_port                = each.value.port
-  to_port                  = each.value.port
-  protocol                 = each.value.protocol
-  source_security_group_id = each.value.destination
-  security_group_id        = aws_security_group.claimant_api_kafka_consumer.id
+  for_each          = { for security_group_rule in local.security_group_rules : security_group_rule.name => security_group_rule }
+  description       = "Allow outbound requests to ${each.value.name}"
+  type              = "egress"
+  from_port         = each.value.port
+  to_port           = each.value.port
+  protocol          = each.value.protocol
+  cidr_blocks       = each.value.destination
+  security_group_id = aws_security_group.claimant_api_kafka_consumer.id
 }
