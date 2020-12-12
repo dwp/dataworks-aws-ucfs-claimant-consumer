273d8deb698e42ab025f1a30920b5e133a3834cc
danhill <danhill@digital.uc.dwp.gov.uk>
2020-12-12 20:35:07 +0000

    create Secrets Manager secret for Kafka consumer MySQL credentials
    Signed-off-by: danhill <danhill@digital.uc.dwp.gov.uk>

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "claimant_api_kafka_consumer_sg" {
     arn  = aws_security_group.claimant_api_kafka_consumer.arn
   }
 }
+
+output "secretsmanager_ucfs_claimant_api_kafka_consumer" {
+  value = aws_secretsmanager_secret.claimant_api_kafka_consumer
+}
